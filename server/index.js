8f6c4365b36c2d37850e7a3cc702b6496dd6c23b
cdraper033 <chrisdraper033@gmail.com>
2023-03-25 16:07:22 -0400

    updated things

--- server/index.js
+++ server/index.js
@@ -1,5 +1,15 @@
 const express = require('express')
 const pm = express()
+const mysql_db = require('mysql')
+
+const connect_db = mysql.createConnection(
+    {
+        host: us-east.connect.psdb.cloud,
+        database: thepassvault,
+        user: t6xdrood6l4wk5648qza,
+        password: pscale_pw_8ddUvNZtPMz2ikFOxW3Qfsrkk9Ea5DglWvmdqTSkeTU
+    }
+);
 
 pm.get('/', (request, response) =>
 {
