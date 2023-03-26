db305f5e6a6f9a240b0ce94d72407e32628cb0be
cdraper033 <chrisdraper033@gmail.com>
2023-03-26 10:56:32 -0400

    added stuff

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const connect_db = mysql.createConnection(
         host: us-east.connect.psdb.cloud,
         database: thepassvault,
         user: t6xdrood6l4wk5648qza,
-        password: pscale_pw_8ddUvNZtPMz2ikFOxW3Qfsrkk9Ea5DglWvmdqTSkeTU
+        password: df,
     }
 );
 
