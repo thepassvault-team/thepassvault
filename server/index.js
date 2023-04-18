9e779a498959d2ab87a0fb698ae7d5ec2cc63443
cdraper033 <chrisdraper033@gmail.com>
2023-04-18 19:57:34 -0400

    did a thing

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express') 
 const app = express()
-const mysql2 = require('mysql2')
+const mysql = require('mysql2')
 const cors = require('cors')
 const PORT = 3001
 require('dotenv').config()
